da18d58917af41820310475b61f61bcb344178ea
Dream <sdeepicivil@gmail.com>
2023-05-05 02:25:16 +0530

    Task 8

--- src/ExpenseItem/ExpenseItem.js
+++ src/ExpenseItem/ExpenseItem.js
@@ -1,6 +1,13 @@
-function ExpenseItem(){
-    return <div><h4>Expense Items</h4> <p>Food Rs.10</p><p>Petrol Rs.100</p><p>Movies Rs.200</p></div>
-    
+function ExpenseItem() {
+    const LocationOfExpenditure = 'Delhi';
+  return (
+    <div>
+      <h4>Expense Items</h4> <p>Food Rs.10</p>
+      <p>Petrol Rs.100</p>
+      <p>Movies Rs.200</p>
+      <p>{LocationOfExpenditure}</p>
+    </div>
+  );
 }
 
 export default ExpenseItem;
