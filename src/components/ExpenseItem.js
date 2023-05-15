f93c6855c10bb389ff749524f60cf9582f44cf3e
Dream <sdeepicivil@gmail.com>
2023-05-15 15:45:30 +0530

    Advanced react task 1

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,10 +4,15 @@ import Card from '../UI/card'
 
 
 function ExpenseItem(props) {
+
+  const deleteHandler = () => {
+    console.log('Clicked');
+  }
   return (
     <Card className='expense-items'>
       <ExpenseDate date={props.date}/>
       <ExpenseDetails amount={props.amount} location={props.location} />
+      <button onClick={deleteHandler}>DELETE</button>
     </Card>
   );
 }
